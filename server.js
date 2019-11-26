f3778ad7cdc6dff029fd9d3a0a31093eb5bd1ede
Mehedi Hasan <mehedi.rucse@gmail.com>
2019-11-27 02:45:43 +0600

    final commitment h

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ app.get('/', (req, res) => {
 // Require Notes routes
 require('./app/routes/note.routes.js')(app);
 
-var port = process.env.PORT || 3000
-    // listen for requests
+var port = process.env.PORT || 3000;
+// listen for requests
 app.listen(port, () => {
     console.log("Server is listening on port 3000");
 });
